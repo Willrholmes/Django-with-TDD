a011a036229d69b214c3aea2d3b82beaaddef243
Willrholmes01 <will.r.holmes@gmail.com>
2016-11-30 18:55:08 +0000

    Added second functional test for second user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -17,7 +17,7 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Rosie has heard about a cool new online to-do app. She goes to checkout
         #its homepage
         self.browser.get(self.live_server_url)
@@ -50,6 +50,41 @@ class NewVisitorTest(LiveServerTestCase):
         # Rosie wonders whether the site will remember her list. Then she sees that the
         # site has generated a unique URL for her -- there is some explanatory text to
         # that effect.
-        self.fail('Finish the test!')
-
         # She visits the URL - her to do list is still there.
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        #Rosie starts a new todo list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy Will\'s present')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy Will\'s present')
+
+        # she notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # New user, Ned, comes along to the site
+
+        ## We use a new browser sesseion to make sure that no information
+        ## of Rosie's is coming from cookies etc.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Ned visit the home page. There is no sign of Rosie's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy Will\'s present', page_text)
+        self.assertNotIn('Buy Will another present', page_text)
+
+        # Ned starts a new list by entering a new item. He
+        # is less interesting than Rosie...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1. Buy milk')
+
+        # Again there is no trace of Rosie's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy Will\'s present', page_text)
+        self.assertIn('Buy milk', page_text)
